822e5e31016d5d661c910a871da18231f4ae7218
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-14 23:27:34 +0500

    codegen practice

---
+++ tests/03 - Using codegen/codegen.spec.js
@@ -0,0 +1,7 @@
+// To use codegen run this command 'npx playwright codegen'
+// a browser and playwright inspector will be opened after running this command
+// in browser you can do actions on web & the script will be generated in playwright inspector
+// you can copy the script and past it in the code editor.
+
+// Or you can run this command to save the script in tests folder or any folder in your project without copy pasting it
+// 'npx playwright codegen -o tests/'name of test file'.

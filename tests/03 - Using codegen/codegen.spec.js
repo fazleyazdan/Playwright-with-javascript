117c04927b251e273c1cf53dba151fd17888aa13
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-15 00:22:12 +0500

    codegen practice

--- tests/03 - Using codegen/codegen.spec.js
+++ tests/03 - Using codegen/codegen.spec.js
@@ -5,3 +5,32 @@
 
 // Or you can run this command to save the script in tests folder or any folder in your project without copy pasting it
 // 'npx playwright codegen -o tests/'name of test file'.
+
+// by default the codegen will open chromium browser, if you wanna change it.
+// 'npx playwright codegen -b firefox' or give initial only 'npx playwright codegen -b ff'
+
+// similarly if you wanna record your test cases using mobile devices
+// npx playwright codegen --device iphone 13
+
+// if you wanna record your script in different view port 
+// npx playwright codegen --viewport-size "1280, 720"
+
+import {test, expect } from '@playwright/test';
+
+test.use({
+    viewport: {
+      height: 720,
+      width: 1280
+    }
+  });
+  
+  test('test', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com/');
+    await page.getByRole('link', { name: 'Log in' }).click();
+    await page.locator('#loginusername').click();
+    await page.locator('#loginusername').fill('fazleyazdan');
+    await page.locator('#loginpassword').click();
+    await page.locator('#loginpassword').fill('123@123');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: 'Log out' }).click();
+  });

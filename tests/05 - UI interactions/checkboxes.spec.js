239378931b6d6060a8a685b6be27cc510db8e5c4
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-18 23:14:22 +0500

    check boxes

--- tests/05 - UI interactions/checkboxes.spec.js
+++ tests/05 - UI interactions/checkboxes.spec.js
@@ -5,18 +5,35 @@ test('check boxes', async ({page}) => {
 
     await page.goto("https://testautomationpractice.blogspot.com/")
 
-    // single checkbox
+    // select single checkbox
     await expect(await page.locator('#wednesday')).toBeVisible()
-    await page.locator('#wednesday').check()                         // check the checkbox
+    await page.locator('#wednesday').check()                     // check the checkbox
 
-
-    await expect(await page.locator('#male')).toBeChecked()     // Validate Check box is checked
+    await expect(await page.locator('#wednesday')).toBeChecked()
+    await expect(await page.locator('#wednesday').isChecked()).toBeTruthy()
     
-    // some other assertions
-    await expect(await page.locator('#male').isChecked()).toBeTruthy()     // Code Commentary below
-    await expect(await page.locator('#female').isChecked()).toBeFalsy()    // Code Commentary below
-   
-    await page.waitForTimeout(5000)            // wait for 5 sec before closing the browser
+    // select all checkboxes having same locators
+    // here we will select all check boxes at once which has the same locator
+
+    const checkboxes_list = await page.$$("//input[contains(@value,'day')]")
+
+    for (const checkbox of checkboxes_list)
+    {
+        await checkbox.check()
+    }
+
+    await page.waitForTimeout(2000)
+
+    // select multiple checkboxes having different locators
+    // first we will store those locators in an array
+    
+    const checkboxes_list1 = ['#monday', '#tuesday', '#wednesday']
+
+    for (const checkboxes of checkboxes_list1)
+    {
+        await page.locator(checkboxes).uncheck()             // uncheck them as they are already checked.
+    }
+    await page.waitForTimeout(5000)            
 
 
 })

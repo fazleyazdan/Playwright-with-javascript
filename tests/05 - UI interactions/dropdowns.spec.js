fa573a4674fcbd5c7ef6d9908f30f4a190d73d99
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-19 16:06:31 +0500

    drop downs assertions

--- tests/05 - UI interactions/dropdowns.spec.js
+++ tests/05 - UI interactions/dropdowns.spec.js
@@ -13,10 +13,32 @@ test('drop downs', async ({page}) => {
     await page.locator('#country').selectOption('United Kingdom')
     await page.waitForTimeout(2000)
 
+    await page.selectOption('#country', 'Japan')         // select dropdowns by text but without using locator method
+    await page.waitForTimeout(2000)
+
     // select by dropdown by label, which is also the text of the dropdown 
     await page.locator('#country').selectOption({label: 'India'})
     await page.waitForTimeout(2000)
 
-    // select 
-    
+    // select dropdown having value attribute
+    await page.locator('#country').selectOption({value: 'canada'})
+    await page.waitForTimeout(2000)
+
+    // select dropdown by index
+    await page.locator('#country').selectOption({index: 3})
+    await page.waitForTimeout(2000)
+
+
+    //! Assertions on dropdowns
+
+    // compare the number of known options with that on the web
+    const numOptions = await page.locator('#country options')
+    await expect(numOptions).toHaveCount(10)
+
+    // check number of options in dropdowns
+    const options = await page.$$('#country options') 
+    console('total number of options in dropdown: ', options.length)             // print length of the options
+    await expect(options.length).toBe(10)                                        // validations on options length
+
+    await page.waitForTimeout(2000)
 })

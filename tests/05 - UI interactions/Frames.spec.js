268823728c604f7a7662891749514cb3f788f6c3
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-26 15:33:35 +0500

    Handling frame

--- tests/05 - UI interactions/Frames.spec.js
+++ tests/05 - UI interactions/Frames.spec.js
@@ -13,9 +13,17 @@ test('Handling Frames', async({page}) => {
     const totalFrames = await page.frames()
     console.log("Total num of Frames:", totalFrames.length)
 
-    // Approach 1: locate Frame through Frame Locator
+    //! Approach 1: locate Frame through Frame Locator
+    //* in this approach we will locate frame & then interact with its elements
+
     const inputBox = await page.frameLocator("frame[src='frame_1.html']").locator("input[name='mytext1']")
     await inputBox.fill('Hello')
 
-    // Approach Number 2:
+    //! Approach Number 2: locate frame through name & URL
+    //* to find URL of the frame, inspect the frame. it will have the 'src' attribute. right click on its value & copy the link address
+
+    const frame1 = await page.frame({url:"https://ui.vision/demo/webtest/frames/frame_2.html"})
+    await frame1.fill("input[name='mytext2']", 'Hello Frame2')   // first parameter is the locator of the input box & second is the text we wanna type into the box
+
+    await page.waitForTimeout(3000)
 })

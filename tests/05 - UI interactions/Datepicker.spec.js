da183ee1df381824453d71d0699afa170cd9a564
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-21 23:01:28 +0500

    datepicker

--- tests/05 - UI interactions/Datepicker.spec.js
+++ tests/05 - UI interactions/Datepicker.spec.js
@@ -6,5 +6,33 @@ import {test, expect } from '@playwright/test';
 
 test('Date Picker', async({page})=> {
 
-    page.goto("")
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    //* Approach 1 : through fill() - pass the date
+
+    // await page.fill('#datepicker', '07/07/1997')
+
+    //* Approach 2 : through loop
+    
+    const year = '2025'
+    const month = 'August'
+    const day = '17'
+
+    await page.click("#datepicker")
+
+    while(true)
+    {
+        const currentYear = await page.locator(".ui-datepicker-year").textContent()
+        const currentMonth = await page.locator(".ui-datepicker-month").textContent()
+        
+        if (currentYear == year && currentMonth == month)
+        {
+            break                      // break the loop b/c current year & month matched our expected year & month
+        }
+
+        await page.click(".ui-icon.ui-icon-circle-triangle-e")               // click next until we have expected year & month
+    }
+
+
+    await page.waitForTimeout(3000)
 })

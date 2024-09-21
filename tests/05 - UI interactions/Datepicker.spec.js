cb087e355f18b9996c736faf47dafb2c48d773bb
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-21 22:13:13 +0500

    datepicker

--- tests/05 - UI interactions/Datepicker.spec.js
+++ tests/05 - UI interactions/Datepicker.spec.js
@@ -1,3 +1,7 @@
+//! Date picker is customized element
+// meaning people customize according to their choice & needs
+// so date picker will not be same for every web
+
 import {test, expect } from '@playwright/test';
 
 test('Date Picker', async({page})=> {

32038e24fccd5bed9f2e14af8daf33413f78ab5e
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-24 17:33:57 +0500

    mouse hover

---
+++ tests/07 - mouse actions/mouse_hover.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Mouse Hover', async({page}) => {
+
+    await page.goto("https://stqatools.com/demo/MouseHover.php")
+
+    const dropdown = await page.locator('.dropbtn')
+    const link2 = await page.locator("//a[normalize-space()='Link 2']")
+
+    //* use hover method to hover over elements
+    dropdown.hover()
+    link2.hover()
+
+    await page.waitForTimeout(2000)
+})

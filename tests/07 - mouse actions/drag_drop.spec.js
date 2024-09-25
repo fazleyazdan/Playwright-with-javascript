e49ce7bf751b07917726a6ed08bddc3779e1d443
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-25 13:12:23 +0500

    mouse drag & drop

--- tests/07 - mouse actions/drag_drop.spec.js
+++ tests/07 - mouse actions/drag_drop.spec.js
@@ -6,11 +6,22 @@ test('login - valid credentials', async({page}) => {
 
     // drag: rome --> italy
 
-    const rome = await page.locator("")
-    const italy = await page.locator("")
+    const rome = await page.locator("#box6")
+    const italy = await page.locator("#box106")
 
     // Approach 1:
-
+    // we will hover over rome & will use down method then hover over italy & will use up method
+    await rome.hover()
+    await page.mouse.down()
+    await italy.hover()
+    await page.mouse.up()
 
     // Approach 2:
+    const madrid = await page.locator("#box7")
+    const spain = await page.locator("#box107")
+
+    // drag: Madrid --> spain
+    await madrid.dragTo(spain)
+
+    await page.waitForTimeout(1000)
 })

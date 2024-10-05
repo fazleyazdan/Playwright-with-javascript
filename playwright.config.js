b2da130d543048bc587b251bdbd96fd9cfd32fb4
fazleyazdan <fazleyazdan345@gmail.com>
2024-10-05 14:11:52 +0500

    configuration changed

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: "retain-on-failure",
     // video: "on",
     // screenshot: "on" 
   },

59a21ea910121b57e63d507220ea8f0ded71f498
fazleyazdan <fazleyazdan345@gmail.com>
2024-10-15 22:31:45 +0500

    page object model

--- tests/12 - page objects/pom.spec.js
+++ tests/12 - page objects/pom.spec.js
@@ -1,16 +1,18 @@
 import { test,expect } from '@playwright/test';
 import { LoginPage } from '../../pages/loginPage';
 import { HomePage } from '../../pages/homePage';
-import { waitForDebugger } from 'inspector';
+import { CartPage } from '../../pages/cartPage';
+import exp from 'constants';
 
 test('page objects demo', async({page}) => {
 
      // first make object of all classes
+     //  pass page to all constructors
      const login = new LoginPage(page)
      const home = new HomePage(page)
+     const cart = new CartPage(page)
      
      // Login 
-
     await login.visitWebPage()
     await login.loginToWeb('admin777', 'admin123')
 
@@ -18,7 +20,12 @@ test('page objects demo', async({page}) => {
 
     // Homepage: add product to cart
     await home.addProductCart("Nexus 6")
+    await page.waitForTimeout(2000)
+    await home.gotoCart()
+    await page.waitForTimeout(5000)
 
+    // CartPage : check if the products are added
+    const status = await cart.checkIfProductAdded("Nokia lumia 1520")
+    await expect(status).toBe(true)
     await page.waitForTimeout(4000)
-
 })

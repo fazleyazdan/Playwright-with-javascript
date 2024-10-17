ea7739b12f5c9322b1483c7a6a763727b10cd26d
fazleyazdan <fazleyazdan345@gmail.com>
2024-10-17 11:16:03 +0500

    built in reporters

---
+++ tests/14 - reporters/built_reporters.spec.js
@@ -0,0 +1,2 @@
+// Playwright Test comes with a few built-in reporters for different needs and ability to provide custom reporters.
+// They include : List, Line, Dot, HTML, Blob, JSON & Junit reporters

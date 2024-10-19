0edb757231586419e2804fea720f10a7d234e2bd
fazleyazdan <fazleyazdan345@gmail.com>
2024-10-19 22:41:46 +0500

    trace viewer

--- tests/03 - codegen & traceviewer/traceviewer.spec.js
+++ tests/03 - codegen & traceviewer/traceviewer.spec.js
@@ -1,10 +1,13 @@
-// by using traceviewer we can trace our tests
-// traceviewer is useful for tests which are usually prone to failing
-// it contains everything about a test: timestamps, screenshots, networks etc
-//* to enable trace viewer : go to config file -> navigate to 'use' block.
-//* there we have 'trace' option. you can turn it on for all tests or you can do it for failed tests only
-//* by default the trace viewer file is stored in test-results folder
+/*  Trace viewer & flaky tests
+ 
+by using traceviewer we can trace our tests
+ traceviewer is useful for tests which are usually prone to failing
+ it contains everything about a test: timestamps, screenshots, networks etc
+ to enable trace viewer : go to config file -> navigate to 'use' block.
+ there we have 'trace' option. you can turn it on for all tests or you can do it for failed tests only
+ by default the trace viewer file is stored in test-results folder
 
+ */
 
 import {test,expect} from '@playwright/test';
 
@@ -18,3 +21,15 @@ test('checking trace viewer', async({page}) => {
     await page.waitForTimeout(1000)
 })
 
+
+/* 
+
+* Flaky test & retries:
+
+* sometimes the test are failing and passing (flaky tests).
+* when a test fails you wanna run it again for the flaky tests.
+* run this command `npx playwright test abc.spec.js --retries=2`
+* you can give as many retries you want. so playwright will rerun the failed test until run out of retries
+* To test retries, intentionally interrupt a test in headed mode by clicking something in UI so that test fail, then playwright will rerun it. If the --retries flag is passed.
+
+*/
